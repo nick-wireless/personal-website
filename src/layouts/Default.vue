a5b2efbc57bee94fc997cc8d1d3027e40521ed99
Nick Hanigan <australian.nick@gmail.com>
2019-09-15 14:50:00 +0930

    Show Small Menu
    Learnt: how to apply theme(colors.cp1.1000) to a Fill CSS property.
    Sub-menu formatting & mechanics.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,14 +3,29 @@
     <header class="text-cp1-800 pt-10">
       <div class="flex justify-between items-center">
         <g-link class="text-lg font-semibold" to="/">Nick Hanigan</g-link>
-        <MenuIcon class="fill-current logo md:hidden"></MenuIcon>
+        <div v-on:click="showSmallMenu = !showSmallMenu">
+          <MenuIcon
+            v-if="showSmallMenu"
+            class="fill-current logo md:hidden"
+          ></MenuIcon>
+          <ExitIcon v-else class="logo md:hidden"></ExitIcon>
+        </div>
       </div>
+      <nav v-if="!showSmallMenu" class="md:hidden">
+        <g-link class="pt-8 block text-base font-medium" to="/">About</g-link>
+        <g-link class="pt-4 block text-base font-medium" to="/hopscotch"
+          >Hopscotch</g-link
+        >
+        <g-link class="py-4 block text-base font-medium" to="/images"
+          >Images</g-link
+        >
+      </nav>
       <nav
         class="hidden md:py-6 md:block md:w-full md:flex md:justify-start md:text-base md:border-b-2 md:font-semibold"
       >
         <g-link class="pl-2 pr-6" to="/">About</g-link>
         <g-link class="px-6" to="/hopscotch">Hopscotch</g-link>
-        <g-link class="px-6" to="/blog">Images</g-link>
+        <g-link class="px-6" to="/images">Images</g-link>
       </nav>
     </header>
     <slot />
@@ -27,24 +42,43 @@ query {
 
 <script>
 import MenuIcon from "~/assets/svgs/iconMenu.svg";
+import ExitIcon from "~/assets/svgs/icon-close-square.svg";
 export default {
   name: "Default Layout",
   components: {
-    MenuIcon
+    MenuIcon,
+    ExitIcon
+  },
+  data() {
+    return {
+      showSmallMenu: true
+    };
+  },
+  methods: {
+    toggleMenu: function() {
+      return !showSmallMenu;
+    }
   }
 };
 </script>
 
 <style>
 body {
-  @apply font-sans antialiased bg-teal-100 text-cp1-900;
+  @apply font-sans antialiased bg-gray-100 text-base text-cp1-900;
 }
 .active {
-  @apply text-cp1-1000;
+  @apply text-cp1-1000 font-semibold;
 }
 .logo {
   width: 24px;
   height: 24px;
   /* margin-right: 10px; */
+}
+.primary {
+  fill: theme(colors.cp1.300);
+}
+
+.secondary {
+  fill: theme(colors.cp1.1000);
 }
 </style>
