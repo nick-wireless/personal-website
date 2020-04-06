fa71c0ea972071927b40c1bcd8102a27828b1e58
Nick Hanigan <australian.nick@gmail.com>
2020-04-06 11:59:34 +0930

    Menu Configure
    Including profile icon.
    Open item: working on link (indicating action) ~ currently not sizing well, wrong colour and too big.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,7 +2,15 @@
   <div class="max-w-3xl mx-auto px-6 md:px-8">
     <header class="text-cp1-800 pt-10">
       <div class="flex justify-between items-center">
-        <g-link class="text-lg font-semibold" to="/">Nick Hanigan</g-link>
+        <div id="profile-pic" class="flex items-center md:flex-col ">
+          <div class="pr-3 md:mb-4 md:pl-4">
+            <g-image
+              src="~/assets/images/nickwithnacho.jpg"
+              class=" w-10 h-10 rounded-full object-cover object-top shadow-lg md:w-24 md:h-24"
+            ></g-image>
+          </div>
+          <g-link class="text-lg font-semibold" to="/">Nick Hanigan</g-link>
+        </div>
         <div v-on:click="showSmallMenu = !showSmallMenu">
           <MenuIcon
             v-if="showSmallMenu"
@@ -11,20 +19,20 @@
           <ExitIcon v-else class="logo md:hidden"></ExitIcon>
         </div>
       </div>
-      <nav v-if="!showSmallMenu" class="md:hidden">
-        <g-link class="pt-8 block text-base font-medium" to="/">About</g-link>
-        <g-link class="pt-4 block text-base font-medium" to="/hopscotch"
-          >Hopscotch</g-link
+      <nav v-if="!showSmallMenu" class="md:hidden border-b-2 ">
+        <g-link class="pt-4 block text-base font-medium" to="/">About</g-link>
+        <g-link class="pt-4 block text-base font-medium" to="/projects"
+          >Projects</g-link
         >
-        <g-link class="py-4 block text-base font-medium" to="/images"
+        <g-link class="pt-4 pb-8 block text-base font-medium" to="/images"
           >Images</g-link
         >
       </nav>
       <nav
-        class="hidden md:py-6 md:block md:w-full md:flex md:justify-start md:text-base md:border-b-2 md:font-semibold"
+        class="hidden py-6 md:block  md:flex md:text-base md:border-b-2 md:font-semibold"
       >
         <g-link class="pl-2 pr-6" to="/">About</g-link>
-        <g-link class="px-6" to="/hopscotch">Hopscotch</g-link>
+        <g-link class="px-6" to="/projects">Projects</g-link>
         <g-link class="px-6" to="/images">Images</g-link>
       </nav>
     </header>
@@ -64,7 +72,7 @@ export default {
 
 <style>
 body {
-  @apply font-sans antialiased bg-gray-100 text-base text-cp1-900;
+  @apply font-sans antialiased text-base text-cp1-900;
 }
 .active {
   @apply text-cp1-1000 font-semibold;
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,35 +1,33 @@
 <template>
   <Layout>
     <body>
-      <p class="mt-6 sm:mt-8 md:mt-10 leading-relaxed">
-        Hi! I'm Nick Hanigan from Australia's smallest Capital. I sailed the
-        <g-link class="link-internal">seas</g-link> and travelled the
-        <g-link class="link-internal">outback</g-link> before beginning an
-        <g-link class="link-internal">international</g-link> work career.
+      <p class="mt-12 sm:mt-8 md:mt-10 leading-relaxed">
+        Hi! I'm Nick Hanigan from Darwin, Australia. Many people know me as the
+        maker of Watashino Soap, the travel case for soap in your pocket.
       </p>
       <p class="mt-6 sm:mt-8 md:mt-10 leading-relaxed">
         I strive to be a creative, working on some cool things across very
         different time scales:
         <g-link class="link-internal">short</g-link> ,
         <g-link class="link-internal">medium</g-link> and
-        <g-link class="link-internal">long</g-link>. Many people know me as the
-        person behind the Hopscotch Wallet & App, a folding case for 2 (two) x
-        mobile phones. See here for the official
-        <g-link class="link-internal">hopscotch website</g-link>.
+        <g-link class="link-internal">long</g-link>. I have an international
+        work career focusing on development (property, independent manufacturing
+        & fullstack programming). Prior to this, I sailed the seas and travelled
+        the outback to earn an apprenticeship of sorts.
       </p>
       <p class="mt-6 sm:mt-8 md:mt-10 leading-relaxed">
         I rarely spend time on the socials and only check my personal inbox once
-        every 2 weeks or so. That said, a
-        <g-link class="link-internal">photo journal</g-link> that somewhat
-        describes 'me' is attached and I am am available to
-        <g-link class="link-internal">email direct</g-link> anytime.
+        every 2 weeks or so. That said, you can find a journal that somewhat
+        describes 'me' by
+        <g-link class="link-internal">photos here</g-link> and I am am available
+        to <g-link class="link-internal">email direct here</g-link> anytime.
       </p>
       <p class="mt-6 sm:mt-8 md:mt-10 leading-relaxed">
         Every 12 monthis I intend to send out an insider's guide to my creative
         year. If I'm busy, I will send it every 6 months, to ensure it is always
-        short and sweet. If you are interested, try
-        <g-link class="link-internal">this link</g-link> to enter
-        <g-link class="link-internal">your email address</g-link>.
+        short and sweet. If you are interested,
+        <g-link class="link-internal">subscribe here</g-link>
+        <Link class="small-icon"></Link>.
       </p>
       <p class="mt-6 sm:mt-8 md:mt-10 leading-relaxed">
         Thanks for your interest. Have a great day!
@@ -39,9 +37,14 @@
 </template>
 
 <script>
+import Link from "~/assets/svgs/icon-external.svg";
+
 export default {
   metaInfo: {
     title: "About"
+  },
+  components: {
+    Link
   }
 };
 </script>
@@ -49,5 +52,8 @@ export default {
 <style>
 .link-internal {
   @apply text-cp1-1000 font-medium border-b border-cp1-1000;
+}
+.small-icon {
+  @apply w-6 h-6;
 }
 </style>
