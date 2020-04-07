1674c75f692eb3c30b530bea9b0c3b3c792a4052
Nick Hanigan <australian.nick@gmail.com>
2020-04-07 09:32:20 +0930

    Link Styling
    Adjusting link presentation for home page.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -19,18 +19,27 @@
         I rarely spend time on the socials and only check my personal inbox once
         every 2 weeks or so. That said, you can find a journal that somewhat
         describes 'me' by
-        <g-link class="link-internal">photos here</g-link> and I am am available
-        to <g-link class="link-internal">email direct here</g-link> anytime.
+        <g-link class="link-internal"
+          >photos<Link class="small-icon"></Link
+        ></g-link>
+        and I am am available to
+        <g-link class="link-internal"
+          >email <Link class="small-icon"></Link
+        ></g-link>
+        anytime.
       </p>
       <p class="mt-6 sm:mt-8 md:mt-10 leading-relaxed">
-        Every 12 monthis I intend to send out an insider's guide to my creative
-        year. If I'm busy, I will send it every 6 months, to ensure it is always
-        short and sweet. If you are interested,
-        <g-link class="link-internal">subscribe here</g-link>
-        <Link class="small-icon"></Link>.
+        I intend to send out an insider's guide to my creative year. If I'm
+        busy, I will send it every 6 months, to ensure it is short and sweet,
+        otherwise every 12 months. If this interests you,
+        <g-link>
+          subscribe here
+          <Link class="small-icon"></Link>
+        </g-link>
+        .
       </p>
       <p class="mt-6 sm:mt-8 md:mt-10 leading-relaxed">
-        Thanks for your interest. Have a great day!
+        A big thankyou for your interest. Have a great day!
       </p>
     </body>
   </Layout>
@@ -51,9 +60,9 @@ export default {
 
 <style>
 .link-internal {
-  @apply text-cp1-1000 font-medium border-b border-cp1-1000;
+  @apply text-cp1-1000 font-medium;
 }
 .small-icon {
-  @apply w-6 h-6;
+  @apply w-5 h-5 inline mb-1 ml-1;
 }
 </style>
